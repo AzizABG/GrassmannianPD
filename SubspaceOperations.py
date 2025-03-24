810c294657335daadb32ee0a2cd0d8dafc58aaec
stravers2 <147452795+stravers2@users.noreply.github.com>
2025-03-24 11:52:58 -0400

    Notes on SubspaceOperations.py

--- SubspaceOperations.py
+++ SubspaceOperations.py
@@ -1,11 +1,16 @@
+#look into scipy linalg operations
+#anything less than 10^-10 should be 0, should be able to do this with null_mask parameter
+
 def IntersectionOfSpaces(B_1, B_2):
   # B_1 and B_2 are basis for two subspaces (of R^n): W_1 and W_2 
   # They are given as 2D numpy arrays (columns of the arrays form the basis)
   # This function returns a basis for the intersection of W_1 and W_2
+  #horizontally stack and then row reduce
 
 def SumOfSpaces(basis_list):
   # the input basis_list consists of lists of numpy arrays (3D numpy array). basis_list[i] is a basis for a subspace W_i of R^n
   # This function returns a basis for the sum of W_1 + W_2 + ... + W_k
+  #vertically stack and then row reduce
 
 def OrthComplement(A,B)
   # A and B are basis for (2D numpy arrays) two subspaces W_A and W_B
