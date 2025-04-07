1817cb6fe44e03eb2f879ef5ff45265dc2cfd49d
Aziz Burak Gülen <51426321+AzizABG@users.noreply.github.com>
2025-04-07 10:32:11 -0400

    Create VietorisRips.py

---
+++ VietorisRips.py
@@ -0,0 +1,6 @@
+def vietoris_rips(D, epsilon, k_max):
+  #This function takes a distance matrix = D, epsilon, and the maximum degree k_max and computes the Vietoris-Rips filtration for each degree 0,1,2,...,k_max 
+
+
+def boundary_maxrix(simplicies_k, simplices_k_minus_1)
+  #This function constructs the real-valued (numoy) boundary matrix from (k)-simplices to (k-1)-simplices.
