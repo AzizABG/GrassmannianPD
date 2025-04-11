e830b5c77d24b42d18b5e6bbc01dc2fdad91bdfb
Reilly Snyder <rjs88D@duke.edu>
2025-04-11 17:08:59 -0400

    VR done

--- VR_tests.py
+++ VR_tests.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.linalg import null_space
 import sympy as sp
 
-from VietorisRips import vietoris_rips
+from VietorisRips import vietoris_rips, boundary_matrix
+
 
 def VR_test():
     D = np.array([
@@ -16,6 +17,12 @@ def VR_test():
     epsilon = 1.5
     k = 3
 
-    print(vietoris_rips(D, epsilon, k))
+    rips_complex = vietoris_rips(D, epsilon, k)
+    print(rips_complex)
+    simplices_2 = rips_complex[2]
+    simplices_1 = rips_complex[1]
 
+# Call the boundary matrix function
+    B2 = boundary_matrix(simplices_2, simplices_1)
+    print(B2)
 VR_test()
--- VietorisRips.py
+++ VietorisRips.py
@@ -1,47 +1,70 @@
 from scipy.spatial.distance import pdist, squareform
 from itertools import combinations
+from itertools import combinations
+from collections import defaultdict
+import numpy as np
+from scipy.linalg import null_space
+import sympy as sp
+
 
 
 def vietoris_rips(D, epsilon, k_max):
-  #This function takes a distance matrix = D, epsilon, and the maximum degree k_max and computes the simplices in the epsilon-Vietoris-Rips complex for each degree 0,1,2,...,k_max 
-  #(it might be better to do this for a fixed k in order to get rid of redundant computations. That is, implement for a single k, and if we need all 0,1,2,...,k_max, we can simply write a loop)
-  n = len(D)
-  print(n)
+    """
+    Computes the Vietoris-Rips complex up to dimension k_max
+    for a given distance matrix D and epsilon.
+    Returns a dictionary mapping each dimension to a list of simplices.
+    """
+    n = len(D)
+    complex_dict = defaultdict(list)
 
-  # 0-simplices: vertices
-  simplices = [tuple([i]) for i in range(n)]
+    # 0-simplices: vertices
+    for i in range(n):
+        complex_dict[0].append((i,))
 
-  # 1-simplices: edges with length ≤ ε
-  edges = []
-  for i in range(n):
-      for j in range(i + 1, n):
-          d = D[i][j]
-          print(d)
-          if d <= epsilon:
-              edges.append(tuple(sorted([i, j])))
-  simplices += edges
+    # 1-simplices: edges with length ≤ ε
+    adjacency = defaultdict(set)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if D[i][j] <= epsilon:
+                edge = (i, j)
+                complex_dict[1].append(edge)
+                adjacency[i].add(j)
+                adjacency[j].add(i)
 
-  # Higher-order simplices
-  from collections import defaultdict
-  adjacency = defaultdict(set)
-  for (i, j) in edges:
-      adjacency[i].add(j)
-      adjacency[j].add(i)
+    # Higher-order simplices
+    def is_clique(vertices):
+        return all(j in adjacency[i] for i, j in combinations(vertices, 2))
 
-  def is_clique(vertices):
-      return all(j in adjacency[i] for i, j in combinations(vertices, 2))
+    for k in range(2, k_max + 1):
+        for combo in combinations(range(n), k + 1):
+            if is_clique(combo):
+                filtration_value = max(D[i][j] for i, j in combinations(combo, 2))
+                if filtration_value <= epsilon:
+                    complex_dict[k].append(combo)
 
-  for k in range(2, k_max + 1):
-      for combo in combinations(range(n), k + 1):
-          if is_clique(combo):
-              filtration_value = max(D[i][j] for i, j in combinations(combo, 2))
-              if filtration_value <= epsilon:
-                  simplices.append((combo))
-
-  return simplices  # List of (simplex, filtration value)
+    return dict(complex_dict)
 
 
 
 
-# def boundary_matrix(simplices_k, simplices_k_minus_1):
+def boundary_matrix(simplices_k, simplices_k_minus_1):
+    """
+    Constructs the real-valued boundary matrix from k-simplices to (k-1)-simplices.
+    """
+    # Mapping from simplex to index for (k-1)-simplices
+    index_map = {tuple(sorted(s)): i for i, s in enumerate(simplices_k_minus_1)}
+    n_rows = len(simplices_k_minus_1)
+    n_cols = len(simplices_k)
+
+    B = np.zeros((n_rows, n_cols))
+
+    for j, simplex in enumerate(simplices_k):
+        for i in range(len(simplex)):
+            face = simplex[:i] + simplex[i+1:]  # remove one vertex
+            sign = (-1) ** i  # orientation sign
+            if face in index_map:
+                row = index_map[face]
+                B[row, j] = sign
+
+    return B
 #   #This function constructs the real-valued (numpy) boundary matrix from (k)-simplices to (k-1)-simplices.
