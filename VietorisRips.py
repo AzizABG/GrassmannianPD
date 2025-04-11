cf775a161853bbe550940207975eb512bbed540c
stravers2 <samir.travers@gmail.com>
2025-04-11 16:15:13 -0400

    VR filtration first draft

--- VietorisRips.py
+++ VietorisRips.py
@@ -1,7 +1,46 @@
+from scipy.spatial.distance import pdist, squareform
+from itertools import combinations
+
+
 def vietoris_rips(D, epsilon, k_max):
   #This function takes a distance matrix = D, epsilon, and the maximum degree k_max and computes the simplices in the epsilon-Vietoris-Rips complex for each degree 0,1,2,...,k_max 
   #(it might be better to do this for a fixed k in order to get rid of redundant computations. That is, implement for a single k, and if we need all 0,1,2,...,k_max, we can simply write a loop)
+  dist = squareform(pdist(D))
+  n = len(D)
+
+  # 0-simplices: vertices
+  simplices = [(tuple([i]), 0.0) for i in range(n)]
+
+  # 1-simplices: edges with length ≤ ε
+  edges = []
+  for i in range(n):
+      for j in range(i + 1, n):
+          d = dist[i, j]
+          if d <= epsilon:
+              edges.append((tuple(sorted([i, j])), d))
+  simplices += edges
+
+  # Higher-order simplices
+  from collections import defaultdict
+  adjacency = defaultdict(set)
+  for (i, j), _ in edges:
+      adjacency[i].add(j)
+      adjacency[j].add(i)
+
+  def is_clique(vertices):
+      return all(j in adjacency[i] for i, j in combinations(vertices, 2))
+
+  for k in range(2, k_max + 1):
+      for combo in combinations(range(n), k + 1):
+          if is_clique(combo):
+              filtration_value = max(dist[i, j] for i, j in combinations(combo, 2))
+              if filtration_value <= epsilon:
+                  simplices.append((combo, filtration_value))
+
+  return simplices  # List of (simplex, filtration value)
 
 
-def boundary_maxrix(simplices_k, simplices_k_minus_1):
+
+
+def boundary_matrix(simplices_k, simplices_k_minus_1):
   #This function constructs the real-valued (numpy) boundary matrix from (k)-simplices to (k-1)-simplices.
