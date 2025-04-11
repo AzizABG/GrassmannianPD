1b2cd461285a92feb2600d01bd92fb5be3d03856
stravers2 <samir.travers@gmail.com>
2025-04-11 16:48:52 -0400

    added tough test case

--- VR_tests.py
+++ VR_tests.py
@@ -5,12 +5,17 @@ import sympy as sp
 from VietorisRips import vietoris_rips
 
 def VR_test():
-    x = np.array([[0, 2, 3], 
-                 [2, 0, 4],
-                 [3, 4, 0]])
-    epsilon = 4
+    D = np.array([
+    [0,   1,   1,   1.4, 3.0, 1.1],
+    [1,   0,   1,   1.4, 2.8, 1.0],
+    [1,   1,   0,   1.4, 2.7, 1.0],
+    [1.4, 1.4, 1.4, 0,   2.9, 1.3],
+    [3.0, 2.8, 2.7, 2.9, 0,   3.2],
+    [1.1, 1.0, 1.0, 1.3, 3.2, 0  ]
+])
+    epsilon = 1.5
     k = 3
 
-    print(vietoris_rips(x, epsilon, k))
+    print(vietoris_rips(D, epsilon, k))
 
 VR_test()
